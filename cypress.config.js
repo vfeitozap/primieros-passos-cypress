2a625ad68e34063ff877b8217b1bea25949631e6
Vinicius <vfeitoza@yahoo.com>
2025-03-29 22:06:40 -0300

    Created baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,7 @@ module.exports = {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+
+    baseUrl: 'https://opensource-demo.orangehrmlive.com/web/index.php',
   },
 };
--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,4 +1,6 @@
-describe('Oragin HRM Specs', () => {
+import userData from '../fixtures/users/userData.json'
+
+describe('Orange HRM Specs', () => {
   
   const selectorList = {
     usernameField: "[name='username']",
@@ -7,40 +9,31 @@ describe('Oragin HRM Specs', () => {
     dashboardGrid: '.orangehrm-dashboard-grid',
     dailogAlert: "[role='alert'] .oxd-alert-content--error"
   }
-  const userData = {
-    userSuccess: {
-      username: "Admin",
-      password: "admin123"
-    },
-    userFail: {
-      username: "Test",
-      password :"test"
-    }
-  }
+ 
   
   it('login-success', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorList.loginButton).click()
     cy.get(selectorList.dashboardGrid)
   })
   it('login-fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorList.usernameField).type(userData.userFail.username)
     cy.get(selectorList.passwordField).type(userData.userFail.password)
     cy.get(selectorList.loginButton).click()
     cy.get(selectorList.dailogAlert)
   })
   it('login-Username Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorList.usernameField).type(userData.userFail.username)
     cy.get(selectorList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorList.loginButton).click()
     cy.get(selectorList.dailogAlert)
   })
   it('login-Password Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorList.passwordField).type(userData.userFail.password)
     cy.get(selectorList.loginButton).click()
