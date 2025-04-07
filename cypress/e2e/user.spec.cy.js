ab6d11ff511d94833c7cb550f385e0cb1cc92104
Vinicius <vfeitoza@yahoo.com>
2025-04-07 20:16:00 -0300

    add random values in user specs chancejs

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -5,10 +5,14 @@ import DashboardPage from '../pages/dashboardPage'
 import myInfoPage from '../pages/myInfoPage'
 import MenuPage from '../pages/menuPage'
 
+const Chance = require('chance')
+
 const loginPage = new LoginPage()
 const dashcCheck = new DashboardPage()
 const infoEmployee = new myInfoPage()
 const personalPage = new MenuPage()
+const chance = new Chance()
+
 
 describe('Orange HRM User Tests', () => {
 
@@ -18,7 +22,8 @@ describe('Orange HRM User Tests', () => {
     dashcCheck.dashboardCheck()
     personalPage.openMyInfoPage()
     infoEmployee.myInfoPage()
-    infoEmployee.personalDetails('John', 'Frederic', 'Smith', '06301659', 'Little John', '001200340044005', '2019-3-20')
+    infoEmployee.personalDetails(chance.first(), chance.letter({casting: 'upper'}), chance.last(), chance.natural({ min: 8, max: 10 }), 'Little John', chance.cpf()
+    , chance.birthday({string: true}))
 
   })
 })
